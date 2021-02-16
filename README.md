09770226870a3704f58ab0cfcf8658a0048edd13
mlaizure <maddi.laizure@gmail.com>
2021-02-16 21:47:44 +0000

    Updated Authors and README and added unit testing for FileStorage

---
+++ README.md
@@ -0,0 +1,41 @@
+# AirBnB clone - The Console
+- Creating command interpreter to manage objects for the AirBnB clone
+- Has a parent class (`BaseModel`) for initialization and json serialization and deserialization of class instances
+- Stores and reloads json string representation from file using abstracted storage engine `FileStorage`
+- Creates all clases for future AirBnB projects (all inherit from `BaseModel`)
+  - User
+  - State
+  - City
+  - Amenity
+  - Place
+  - Review
+- Unittests to validate all classes including storage engine
+
+## Command interpreter
+- How to start:
+  - clone repository
+  - run ./console.py from the repo's base directory
+- How to use:
+  - quit and Ctrl + D to exit console
+  - help to see valid commands and documentation
+  - create: creates a new instance of a class (`$ create BaseModel`)
+  - show: prints stringified instance of a class (`$ show BaseModel 123`)
+  - destroy: deletes an instance (`$ destroy BaseModel 123`)
+  - all: prints strings of all instances (or all instances of a certain class) in JSON file (`$ all` or `$ all BaseModel`)
+  - update: updates an instance attribute (`$ update BaseModel 123 attribute "value"`)
+- Examples:
+```
+$ ./console.py
+(hbnb) create User
+f7479d71-40a2-4ba0-97c1-cc9dc2ea8a35
+(hbnb) show User f7479d71-40a2-4ba0-97c1-cc9dc2ea8a35
+[User] (f7479d71-40a2-4ba0-97c1-cc9dc2ea8a35) {'updated_at': datetime.datetime(2021, 2, 16, 21, 37, 23, 544315), 'created_at': datetime.datetime(2021, 2, 16, 21, 37, 23, 544257), 'id': 'f7479d71-40a2-4ba0-97c1-cc9dc2ea8a35'}
+(hbnb) update User f7479d71-40a2-4ba0-97c1-cc9dc2ea8a35 first_name "Betty"
+(hbnb) all
+["[User] (f7479d71-40a2-4ba0-97c1-cc9dc2ea8a35) {'first_name': 'Betty', 'updated_at': datetime.datetime(2021, 2, 16, 21, 39, 54, 28478), 'created_at': datetime.datetime(2021, 2, 16, 21, 37, 23, 544257), 'id': 'f7479d71-40a2-4ba0-97c1-cc9dc2ea8a35'}"]
+(hbnb) destroy User f7479d71-40a2-4ba0-97c1-cc9dc2ea8a35
+(hbnb) all
+[]
+(hbnb) quit
+$
+```
---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+""" Testing File Storage Module """
+
+import unittest
+import json
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+
+
+class TestFileStorage(unittest.TestCase):
+    """ testing output from FileStorage class """
+
+    @classmethod
+    def setUpClass(cls):
+        """sets up class instances for tests"""
+        cls.BM = BaseModel()
+        cls.BM1 = BaseModel()
+        cls.storage = FileStorage()
+
+    @classmethod
+    def tearDownClass(cls):
+        """deletes instances after tests"""
+        del cls.BM
+        del cls.BM1
+        del cls.storage
+
+    def test_all(self):
+        """test all method of file storage"""
+        objects = self.storage.all()
+        self.assertEqual(objects, {})
+        self.storage.new(self.BM)
+        self.storage.new(self.BM1)
+        objects = self.storage.all()
+        self.assertIsInstance(objects, dict)
+        key = "{}.{}".format(type(self.BM).__name__, self.BM.id)
+        self.assertIn(key, objects)
+        self.assertEqual(self.BM, objects.get(key))
+        key1 = "{}.{}".format(type(self.BM1).__name__, self.BM1.id)
+        self.assertIn(key1, objects)
+        self.assertEqual(self.BM1, objects.get(key1))
+
+    def test_new(self):
+        """test new method of file storage"""
+        objects = self.storage.all()
+        key = "{}.{}".format(type(self.BM).__name__, self.BM.id)
+        self.assertIn(key, objects)
+        self.assertEqual(self.BM, objects.get(key))
+
+    def test_save(self):
+        """test save method of file storage"""
+        objects = self.storage.all()
+        self.storage.save()
+        with open("file.json", 'r') as f:
+            from_file = f.read()
+        self.assertEqual(from_file, json.dumps({key: value.to_dict()
+                                                for key, value in
+                                                objects.items()}))
+
+    def test_reload(self):
+        """test reload method of file storage"""
+        before = self.storage.all()
+        self.storage.reload()
+        after = self.storage.all()
+        self.assertDictEqual(before, after)
